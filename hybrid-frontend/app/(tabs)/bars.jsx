01c7a9dd0000cbf1c2f1257703ee7e6150b169be
CodeByManu <mjtagle2@miuandes.cl>
2024-11-19 15:00:53 -0300

    Show events

--- hybrid-frontend/app/(tabs)/bars.jsx
+++ hybrid-frontend/app/(tabs)/bars.jsx
@@ -1,7 +1,26 @@
-import { Text } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Bars() {
   return (
-    <Text>bars</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bars</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F1DCA7", // Fondo agradable
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#C58100", // Un color llamativo
+    textTransform: "uppercase", // Texto en mayúsculas
+  },
+});
